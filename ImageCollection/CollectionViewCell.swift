9b0176d74b7de408c2991a39d9118566d69cd3cd
Sujanth <sujanth1987@gmail.com>
2017-09-06 20:44:14 +1000

    UICollection view added

---
+++ ImageCollection/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  ImageCollection
+//
+//  Created by Sujanth Sebamalaithasan on 6/9/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
