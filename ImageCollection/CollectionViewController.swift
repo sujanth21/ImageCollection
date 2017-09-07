002c379f481b985226f8d1647a394347302d2379
Sujanth <sujanth1987@gmail.com>
2017-09-07 22:01:28 +1000

    Collection view methods added

--- ImageCollection/CollectionViewController.swift
+++ ImageCollection/CollectionViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 private let reuseIdentifier = "Cell"
 
+var images = ["Image1", "Image2", "Image3", "Image4", "Image5", "Image6"]
+
 class CollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
@@ -18,8 +20,6 @@ class CollectionViewController: UICollectionViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
     }
@@ -43,19 +43,19 @@ class CollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return images.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        
+        cell.cellImage.image = UIImage(named: images[indexPath.row])
     
         return cell
     }
